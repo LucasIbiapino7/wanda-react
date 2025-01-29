c5436a1e63429acf223e77e4a23480790aeb9b21
Lucas Ibiapino <lucas.ibiapino07@gmail.com>
2025-01-28 22:43:25 -0300

    add routes and pages: LoginPage and MacthesPage: not implented

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,27 @@ import Header from "./components/Header/Header";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Function from "./pages/Function";
+import LoginPage from "./pages/LoginPage"
+import MatchesPage from "./pages/MatchesPage"
 
 function App() {
   return (
     <Router>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
+        {/* Rota pública */}
         <Route path="/sobre" element={<About />} />
+        <Route path="/login" element={<LoginPage />} />
+
+       {/* Rota privada (ainda sem proteção) */}
+        <Route path="/" element={<Home />} />
         <Route path="/enviar" element={<Function />} />
+
+        {/* Rota só para Admin (também ainda sem proteção) */}
+        <Route path="/partida" element={<MatchesPage />} />
+
+        {/* Rota default (caso queira redirecionar para /login ou /home) */}
+        {/* <Route path="*" element={<Navigate to="/login" />} /> */}
       </Routes>
     </Router>
   );
---
+++ src/pages/MatchesPage.jsx
@@ -0,0 +1,7 @@
+function MatchesPage(){
+    return(
+        <h1>Matches Page</h1>
+    )
+}
+
+export default MatchesPage;
