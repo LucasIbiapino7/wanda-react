3d8ad7329c6082267cca71069a7904aa673080ac
Cecilia Xav1er <mvalexavier@gmail.com>
2026-09-03 16:53:55 -0300

    chore: organiza assets de materiais por audiência e módulo

--- src/data/materials.js
+++ src/data/materials.js
@@ -1,45 +1,44 @@
-import projetoWanda from "../assets/materiais/projeto-wanda.pdf"
-import variaveis from "../assets/materiais/variaveis.pdf"
-import operadores from "../assets/materiais/operadores.pdf"
-import comandosCondicionais from "../assets/materiais/comandos-condicionais.pdf"
-import funcoesDebug from "../assets/materiais/funcoes-internas-e-debug.pdf"
-import funcoes from "../assets/materiais/funcoes.pdf"
+// aluno
+// modulo 1
+import projetoWanda from "../assets/materiais/ambos/geral/projeto-wanda.pdf"
+import variaveisPDF from "../assets/materiais/ambos/modulo-1/variaveis.pdf"
+import operadoresPDF from "../assets/materiais/ambos/modulo-1/operadores.pdf"
+import funcoesInternasEDebugPDF from "../assets/materiais/ambos/modulo-1/funcoes-internas-e-debug.pdf"
+import condicionaisPDF from "../assets/materiais/ambos/modulo-1/comandos-condicionais.pdf"
+// modulo 2
+import forPDF from "../assets/materiais/ambos/modulo-2/for.pdf"
+import funcoesPDF from "../assets/materiais/ambos/modulo-2/funcoes.pdf"
+import whilePDF from "../assets/materiais/ambos/modulo-2/while.pdf"
+// modulo 3
+import listaPDF from "../assets/materiais/ambos/modulo-3/lista.pdf"
+
+// professor
+// bits
+import bitsPPTX from "../assets/materiais/professor/jogos/bits/bits.pptx"
+import bitsMaterialApoioDOCX from "../assets/materiais/professor/jogos/bits/material-de-apoio-ao-professor-bits.docx"
+import bitsPlanoDeAulaXLSX from "../assets/materiais/professor/jogos/bits/plano-de-aula-bits.xlsx"
+// jokenpo
+import jokenpoPPTX from "../assets/materiais/professor/jogos/jokenpo/jokenpo.pptx"
+import jokenpoMaterialApoioDOCX from "../assets/materiais/professor/jogos/jokenpo/material-de-apoio-ao-professor-jokenpo.docx"
+import jokenpoPlanoDeAulaXLSX from "../assets/materiais/professor/jogos/jokenpo/plano-de-aula-jokenpo.xlsx"
+// modulo 1
+import variaveisPptx from "../assets/materiais/professor/modulo-1/VariaveisPROF.pptx"
+import operadoresPptx from "../assets/materiais/professor/modulo-1/OperadoresPROF.pptx"
+import comandosCondicionaisPptx from "../assets/materiais/professor/modulo-1/CondicionaisPROF.pptx"
+import funcoesDebugPptx from "../assets/materiais/professor/modulo-1/FuncoesEDebugPROF.pptx"
+
+// modulo 2
+import funcoesPptx from "../assets/materiais/professor/modulo-2/FuncoesPROF.pptx"
+import whilePptx from "../assets/materiais/professor/modulo-2/WhilePROF.pptx"
+import forPptx from "../assets/materiais/professor/modulo-2/ForPROF.pptx"
+
+// modulo 3
+import listaPptx from "../assets/materiais/professor/modulo-3/listaPROF.pptx"
+
+
+
+
 
 export const materials = [
-   {
-      id: 1,
-      fileName: "Projeto Wanda.pdf",
-      type: "PDF",
-      url: projetoWanda
-   },
-   {
-      id: 2,
-      fileName: "Variáveis.pdf",
-      type: "PDF",
-      url: variaveis
-   },
-   {
-      id: 3,
-      fileName: "Operadores.pdf",
-      type: "PDF",
-      url: operadores
-   },
-   {
-      id: 4,
-      fileName: "Comandos Condicionais.pdf",
-      type: "PDF",
-      url: comandosCondicionais
-   },
-   {
-      id: 5,
-      fileName: "Funções internas e Debug.pdf",
-      type: "PDF",
-      url: funcoesDebug
-   },
-   {
-      id: 6,
-      fileName: "Funções",
-      type: "PDF",
-      url: funcoes
-   }
+   
 ]
